336ba63af972aac55fd6af4838236318e1476d8e
Thomas Rix <trix@dr.dk>
2020-03-19 11:33:43 +0100

    Starting arcs 2 - fixing noise distribution

--- src/script/index.js
+++ src/script/index.js
@@ -3,7 +3,8 @@ import {create, select} from './utils/trix';
 // import './sketches/walker-01';
 // import './sketches/walker-02';
 // import './sketches/walker-03';
-import './sketches/arcs-01';
+// import './sketches/arcs-01';
+import './sketches/arcs-02';
 // import './sketches/tweener';
 
 
