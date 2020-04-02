b256f199b6cab8507ca31b145b17d1afe26e811d
Thomas Rix <trix@dr.dk>
2020-04-02 12:24:24 +0200

    More circle playing

--- src/script/index.js
+++ src/script/index.js
@@ -4,7 +4,8 @@ import {create, select} from './utils/trix';
 // import './sketches/walker-02';
 // import './sketches/walker-03';
 // import './sketches/arcs-01';
-import './sketches/arcs-02';
+// import './sketches/arcs-02';
+import './sketches/arcs-03';
 // import './sketches/tweener';
 
 
